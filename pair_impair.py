f30f170170db7ce7073921717c42d6423c6ed2b2
ThomTrooper <133505004+ThomTrooper@users.noreply.github.com>
2023-05-25 15:55:57 +0200

    Création de : pair_impair.py

---
+++ pair_impair.py
@@ -0,0 +1,10 @@
+def pair_impair(nbr):
+    """
+    Fonction renvoyant si un nombre est pair ou impair
+    Entrée : <int> or <float>
+    Sortie : <str>
+    """
+    if nbr%2==0:
+        return "Pair"
+    else:
+        return "Impair"
